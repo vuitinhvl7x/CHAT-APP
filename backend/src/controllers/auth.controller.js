2c92f6e63d2e0a8d37b764f00b6aeb38ad706b76
Xuan_Hai <xuanhai2708@gmail.com>
2024-12-27 02:50:25 +0300

    Initial project

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("aaa");
+};
+
+export const login = (req, res) => {
+  res.send("aaa");
+};
+
+export const logout = (req, res) => {
+  res.send("aaa");
+};
